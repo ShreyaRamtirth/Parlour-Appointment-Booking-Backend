1f7fefba4a486b03ea137c64951690aa02b37043
Vikas <tondev23@gmail.com>
2022-07-20 17:29:44 +0530

    Added Boilerplate code of all

---
+++ model/user.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const appointment = new Schema({
+    date: Date,
+    time: String
+});
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    forgot: {
+        type: Boolean,
+        default: false
+    },
+    otp: {
+        type: Number,
+        required: false
+    },
+    appointments: [appointment]
+});
+
+export const User = mongoose.model('User', userSchema);
---
+++ routes/publicRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import pkg from "jsonwebtoken";
+
+const Jwt = pkg;
+const router = express.Router();
+
+
+export default router;

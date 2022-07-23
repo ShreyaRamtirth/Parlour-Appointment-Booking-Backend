e18ec7f6bb51f4313cf0498870c9c1e1cbf00fb2
Vikas <tondev23@gmail.com>
2022-07-23 20:36:04 +0530

    Done Password encryption and login

---
+++ middleware/auth.js
@@ -0,0 +1,36 @@
+import pkg from "jsonwebtoken";
+
+import { User } from "../model/users.js";
+
+const Jwt = pkg;
+
+const requireAuth = async (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json({ error: 'No credentials sent!' });
+    }
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).send({
+            messaage: "Unauthenticated"
+        });
+    }
+    var dtoken;
+    await Jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+        if (err) {
+            return res.status(401).send({
+                messaage: "Unauthenticated" + err.messaage
+            });
+        }
+        dtoken = decodedToken;
+    });
+    const user = await User.findOne({ _id: dtoken._id });
+
+    if (!user) {
+        return res.status(404).send({
+            messaage: "User not found"
+        });
+    }
+    next();
+}
+
+export default requireAuth;
--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,20 +1,53 @@
 import express from "express";
 import pkg from "jsonwebtoken";
+import bcrypt from 'bcryptjs';
+
+
 import { User } from '../model/user.js'
 
 const Jwt = pkg;
 const router = express.Router();
 router.post('/register', async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
         role: req.body.role
     });
-    const result = await user.save();
-    const { password, ...data } = result.toJSON();
-    res.send(data);
+    await user.save((err, result) => {
+        if (err) {
+            return res.status(424).send({
+                messaage: "Register unsuccessfull"
+            });
+        }
+        const { password, ...data } = result.toJSON();
+        return res.send(data);
+    });
 });
 
+
+router.post("/login", (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(404).send({
+            messaage: "user not found"
+        });
+    }
+    if (!await bcrypt.compare(req.body.password, user.password)) {
+        return res.status(400).send({
+            messaage: "Invalid Credentials"
+        });
+    }
+    const token = Jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: 86400 // expires in 24 hours
+    });
+    return res.status(400).send({
+        jwt: token,
+        role: user.role,
+        name: user.name
+    });
+});
 
 export default router;

ebf7579ead5093332eefa82b8f7b7f5a42250e9c
Vikas <tondev23@gmail.com>
2022-08-05 11:43:59 +0530

    Book an appoinment

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,6 +1,18 @@
 import express from "express";
+import pkg from "jsonwebtoken";
+import { User } from "../model/users.js";
+const Jwt = pkg;
 
 const router = express.Router();
 
+router.post("/bookapppointment", async (req, res) => {
+    const token = req.headers.authorization;
+    var appointment = req.body.appointment;
+    const claims = await Jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOneAndUpdate({ _id: claims._id }, {
+        $push: { appointments: appointment }
+    });
+
+});
 
 export default router;
